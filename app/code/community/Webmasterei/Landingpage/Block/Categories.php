b17ce62741391e88f27f961bc02982909d2033a3
Bernhard Prange <info@webmasterei-prange.de>
2016-10-05 09:11:24 +0200

    category slider

--- app/code/community/Webmasterei/Landingpage/Block/Categories.php
+++ app/code/community/Webmasterei/Landingpage/Block/Categories.php
@@ -36,14 +36,7 @@ class Webmasterei_Landingpage_Block_Categories
     extends Mage_Core_Block_Html_Link
     implements Mage_Widget_Block_Interface
 {
-    /**
-     * Initialize entity model
-     */
-    protected function _construct()
-    {
-        parent::_construct();
-        $this->_entityResource = Mage::getResourceSingleton('catalog/product');
-    }
+
 
 
 
