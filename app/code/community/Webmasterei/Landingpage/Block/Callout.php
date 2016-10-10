c9fdab95a6230c8a5bcd7194717cb5d48c3f0947
Bernhard Prange <info@webmasterei-prange.de>
2016-10-10 12:54:50 +0200

    callout added

--- app/code/community/Webmasterei/Landingpage/Block/Callout.php
+++ app/code/community/Webmasterei/Landingpage/Block/Callout.php
@@ -1,6 +1,6 @@
 <?php
 // app/code/local/Envato/WidgetLinks/Block/Links.php
-class Webmasterei_Landingpage_Block_Slider
+class Webmasterei_Landingpage_Block_Callout
     extends Mage_Core_Block_Html_Link
     implements Mage_Widget_Block_Interface
 {
