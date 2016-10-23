6aa0b7fac1c56c64387cb7c95fd5e464f8dfef0f
Bernhard Prange <info@webmasterei-prange.de>
2016-10-23 13:32:10 +0200

    Video Widget for Selfhosted and Youtube

---
+++ app/code/community/Webmasterei/Landingpage/Block/Video.php
@@ -0,0 +1,9 @@
+<?php
+// app/code/local/Envato/WidgetLinks/Block/Links.php
+class Webmasterei_Landingpage_Block_Video
+    extends Mage_Core_Block_Template
+    implements Mage_Widget_Block_Interface
+{
+
+
+}
